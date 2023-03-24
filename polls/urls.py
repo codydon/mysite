07ed5ae5db17d9f58f0ba12ff84747825f1af5cf
codydon <donaldonserio20@gmail.com>
2023-03-24 07:29:07 +0300

    .polls debug

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-#     path('', views.index, name='index'),
 #     # ex: /polls/5/
 #     path('<int:question_id>/', views.detail, name='detail'),
 #     # ex: /polls/5/results/
